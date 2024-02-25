e9e24cb2838b40a46a2ca06f98d7f9dceb7a273b
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-02-25 16:40:38 +0100

    25/02/24 *2
    Cambio de estructura de archivos

--- index.mjs
+++ index.mjs
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-import authRoutes from "./routes/authRoutes.mjs";
+import authRoutes from "./src/routes/authRoutes.mjs";
 import { DB_NAME, client } from "./src/config/database.mjs";
-import { PORT } from "../config/constants.mjs";
+import { PORT } from "../src/config/constants.mjs";
 import "dotenv/config";
 
 const app = express();
