ae5fc1c6ecfc09c9c1ef617f705da85f48f2b245
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-02-26 19:05:51 +0100

    26/02/24 *2
    cambios de recolección de movimientos

--- src/controllers/movementsController.mjs
+++ src/controllers/movementsController.mjs
@@ -7,32 +7,30 @@ export async function getMovementsByYear(req, res) {
 
     try {
         const movementsCollection = client.db(DB_NAME).collection("movements");
-        
-        const startDate = new Date(`${year}-01-01T00:00:00.000Z`);
-        const endDate = new Date(`${year}-12-31T23:59:59.999Z`);
 
         const movements = await movementsCollection.find({
             user_id: userId,
-            date: {
-                $gte: startDate.toISOString(),
-                $lte: endDate.toISOString()
-            }
+            date: { $regex: `^${year}-` }
         }).toArray();
 
+        const initialAcc = {};
+        ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"].forEach(month => {
+            initialAcc[month] = { Income: 0, Expenses: 0 };
+        });
+
         const processedData = movements.reduce((acc, movement) => {
-            const month = new Date(movement.date).getMonth();
-            const monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][month];
-            if (!acc[monthName]) {
-                acc[monthName] = { Income: 0, Expenses: 0 };
-            }
+            const monthIndex = parseInt(movement.date.split('-')[1], 10) - 1;
+            const monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][monthIndex];
             const amount = parseFloat(movement.amount.$numberDouble);
+
             if (amount > 0) {
                 acc[monthName].Income += amount;
             } else {
                 acc[monthName].Expenses += Math.abs(amount);
             }
+
             return acc;
-        }, {});
+        }, initialAcc);
 
         const chartData = Object.entries(processedData).map(([month, { Income, Expenses }]) => ({
             month,
