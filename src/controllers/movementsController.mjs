83eb3f07b4d9e66ff8af6c73f3b921ccd3fe9333
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-02-28 19:00:30 +0100

    28/02/24 *14
    prueba 2 getAllMovements

--- src/controllers/movementsController.mjs
+++ src/controllers/movementsController.mjs
@@ -1,13 +1,16 @@
 import { client } from "../config/database.mjs";
 import { DB_NAME } from "../config/constants.mjs";
 
+import { ObjectId } from 'mongodb'; // Importar ObjectId desde el módulo mongodb
+
 export async function getAllMovements(req, res) {
-    const userId = req.user.userId;
+    const userId = req.user.userId; // Obtener el ID del usuario de la solicitud
 
     try {
         const movementsCollection = client.db(DB_NAME).collection("movements");
-        const movements = await movementsCollection.find({ "user_id": userId }).toArray();
+        const movements = await movementsCollection.find({ "user_id": ObjectId(userId) }).toArray();
 
+        // Formatear los datos según el requisito
         const formattedMovements = movements.map(movement => ({
             "user_id": movement.user_id,
             "date": movement.date,
@@ -16,12 +19,13 @@ export async function getAllMovements(req, res) {
             "amount": movement.amount
         }));
 
-        res.json(formattedMovements);
+        res.json(formattedMovements); // Devolver los datos formateados como respuesta
     } catch (error) {
         console.error("Failed to retrieve movements:", error);
         res.status(500).send("Error retrieving movements data");
     }
 }
+
 
 
 export async function getMovementsByYear(req, res) {
