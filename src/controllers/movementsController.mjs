b18083ff0dbe65db3a5fe601c8c84a33afc3af41
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-02-28 19:25:21 +0100

    28/02/24 *18
    getAllMovements y getMovementsByYear FUNCIONANDO OK

--- src/controllers/movementsController.mjs
+++ src/controllers/movementsController.mjs
@@ -3,14 +3,14 @@ import { ObjectId } from 'mongodb';
 import { client } from "../config/database.mjs";
 import { DB_NAME } from "../config/constants.mjs";
 
+const movementsCollection = client.db(DB_NAME).collection("movements");
+
 export async function getAllMovements(req, res) {
     const userId = req.user.userId;
 
     try {
-        const movementsCollection = client.db(DB_NAME).collection("movements");
-
         const movements = await movementsCollection.aggregate([
-            { $match: { "user_id": new ObjectId(userId) }},
+            { $match: { "user_id": new ObjectId(userId) } },
             {
                 $lookup: {
                     from: "categories",
@@ -45,21 +45,36 @@ export async function getMovementsByYear(req, res) {
     const userId = req.user.userId;
 
     try {
-        const movementsCollection = client.db(DB_NAME).collection("movements");
-        const movements = await movementsCollection.find({
-            "user_id": new ObjectId(userId),
-            "date": { $regex: `^${year}` }
-        }).toArray();
+        const movements = await movementsCollection.aggregate([
+            {
+                $match: {
+                    "user_id": new ObjectId(userId),
+                    "date": { $regex: `^${year}` }
+                }
+            },
+            {
+                $lookup: {
+                    from: "categories",
+                    localField: "category",
+                    foreignField: "_id",
+                    as: "categoryInfo"
+                }
+            },
+            {
+                $unwind: "$categoryInfo"
+            },
+            {
+                $project: {
+                    _id: 0,
+                    date: 1,
+                    description: 1,
+                    amount: 1,
+                    category: "$categoryInfo.name"
+                }
+            }
+        ]).toArray();
 
-        const formattedMovements = movements.map(movement => ({
-            "user_id": movement.user_id,
-            "date": movement.date,
-            "category": movement.category,
-            "description": movement.description,
-            "amount": movement.amount
-        }));
-
-        res.json(formattedMovements);
+        res.json(movements);
     } catch (error) {
         console.error("Error retrieving movements by year:", error);
         res.status(500).send("Error retrieving movements data by year");
