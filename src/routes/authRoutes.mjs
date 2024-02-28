7de93f864fca81d738c0395d527340ca7f1e3e77
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-02-28 17:54:13 +0100

    28/02/24 *6.2
    prueba 6.1 getAllMovements

--- src/routes/authRoutes.mjs
+++ src/routes/authRoutes.mjs
@@ -2,12 +2,13 @@ import { Router } from "express";
 
 import { authenticateToken } from '../middlewares/authMiddleware.mjs';
 import { register, login } from "../controllers/authController.mjs";
-import { getMovementsByYear } from '../controllers/movementsController.mjs';
+import { getMovementsByYear, getAllMovements } from '../controllers/movementsController.mjs';
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get('/movements/all', authenticateToken, getAllMovements);
 router.get('/movements/:year', authenticateToken, getMovementsByYear);
 
 export default router;
