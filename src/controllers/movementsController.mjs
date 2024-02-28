6e13e088e969db0e60ccd59e2dabd623d2268b12
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-02-28 18:47:56 +0100

    28/02/24 *12
    Vuelta a categoria con ID

--- src/controllers/movementsController.mjs
+++ src/controllers/movementsController.mjs
@@ -1,51 +1,6 @@
 import { client } from "../config/database.mjs";
 import { DB_NAME } from "../config/constants.mjs";
 
-export async function getMovementsByYear(req, res) {
-    const { year } = req.params;
-    const userId = req.user.userId;
-
-    try {
-        const movementsCollection = client.db(DB_NAME).collection("movements");
-        const movements = await movementsCollection.find({ user_id: userId, date: { $regex: `^${year}-` } }).toArray();
-
-        if (movements.length === 0) {
-            return res.json([]);
-        }
-
-        let monthlyTotals = {};
-        for (let i = 1; i <= 12; i++) {
-            const monthKey = `${year}-${i.toString().padStart(2, '0')}`;
-            monthlyTotals[monthKey] = { Income: 0, Expenses: 0 };
-        }
-
-        movements.forEach(movement => {
-            const monthKey = movement.date;
-            const amount = movement.amount;
-            if (amount > 0) {
-                monthlyTotals[monthKey].Income += amount;
-            } else {
-                monthlyTotals[monthKey].Expenses += Math.abs(amount);
-            }
-        });
-
-        const response = Object.keys(monthlyTotals).map(key => {
-            const monthIndex = parseInt(key.split('-')[1], 10) - 1;
-            return {
-                month: monthNames[monthIndex],
-                Income: Number(monthlyTotals[key].Income.toFixed(2)),
-                Expenses: Number(monthlyTotals[key].Expenses.toFixed(2)),
-            };
-        });
-
-        res.json(response);
-    } catch (error) {
-        console.error("Failed to retrieve movements:", error);
-        res.status(500).send("Error retrieving movements data");
-    }
-}
-
-
 export async function getAllMovements(req, res) {
     const userId = req.user.userId;
 
@@ -67,4 +22,27 @@ export async function getAllMovements(req, res) {
     }
 }
 
+export async function getMovementsByYear(req, res) {
+    const { year } = req.params;
+    const userId = req.user.userId;
+
+    try {
+        const movementsCollection = client.db(DB_NAME).collection("movements");
+        const movements = await movementsCollection.find({
+            user_id: userId,
+            date: { $regex: `^${year}` }
+        }).toArray();
+
+        const formattedMovements = movements.map(movement => ({
+            date: movement.date,
+            category: movement.category,
+            description: movement.description,
+            amount: movement.amount,
+        }));
+        res.json(formattedMovements);
+    } catch (error) {
+        console.error("Failed to retrieve movements:", error);
+        res.status(500).send("Error retrieving movements data");
+    }
+}
 
