8185f8ed0535da09e7ce11ad62cb6f8ab327b0cc
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-02-28 19:09:25 +0100

    28/02/24 *16
    prueba 1 getMovementsByYear con userID > ObjID

--- src/controllers/movementsController.mjs
+++ src/controllers/movementsController.mjs
@@ -25,10 +25,29 @@ export async function getAllMovements(req, res) {
     }
 }
 
-
-
-
 export async function getMovementsByYear(req, res) {
+    const { year } = req.params;
+    const userId = req.user.userId;
 
+    try {
+        const movementsCollection = client.db(DB_NAME).collection("movements");
+        const movements = await movementsCollection.find({ 
+            "user_id": new ObjectId(userId), 
+            "date": { $regex: `^${year}` }
+        }).toArray();
+
+        const formattedMovements = movements.map(movement => ({
+            "user_id": movement.user_id,
+            "date": movement.date,
+            "category": movement.category,
+            "description": movement.description,
+            "amount": movement.amount
+        }));
+
+        res.json(formattedMovements);
+    } catch (error) {
+        console.error("Error retrieving movements by year:", error);
+        res.status(500).send("Error retrieving movements data by year");
+    }
 }
 
