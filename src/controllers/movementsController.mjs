91c7947a5376ef7124335973bf5760a33efe02f2
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-02-28 18:02:19 +0100

    28/02/24 *8
    prueba 1 getMovementsByYear y getAllMovements con nombres de categorias

--- src/controllers/movementsController.mjs
+++ src/controllers/movementsController.mjs
@@ -7,45 +7,67 @@ export async function getMovementsByYear(req, res) {
 
     try {
         const movementsCollection = client.db(DB_NAME).collection("movements");
-        const movements = await movementsCollection.find({
-            user_id: userId,
-            date: { $regex: `^${year}` }
-        }).toArray();
+        const movements = await movementsCollection.aggregate([
+            { $match: { user_id: userId, date: { $regex: `^${year}` } }},
+            {
+                $lookup: {
+                    from: "categories",
+                    localField: "category",
+                    foreignField: "_id",
+                    as: "categoryData"
+                }
+            },
+            {
+                $unwind: "$categoryData"
+            },
+            {
+                $project: {
+                    date: 1,
+                    category: "$categoryData.name",
+                    description: 1,
+                    amount: 1
+                }
+            }
+        ]).toArray();
 
-        const formattedMovements = movements.map(movement => ({
-            date: movement.date,
-            category: movement.category,
-            description: movement.description,
-            amount: movement.amount,
-        }));
-
-        res.json(formattedMovements);
+        res.json(movements);
     } catch (error) {
         console.error("Failed to retrieve movements:", error);
         res.status(500).send("Error retrieving movements data");
     }
 }
-
 
 export async function getAllMovements(req, res) {
     const userId = req.user.userId;
 
     try {
         const movementsCollection = client.db(DB_NAME).collection("movements");
-        const movements = await movementsCollection.find({ user_id: userId }).toArray();
+        const movements = await movementsCollection.aggregate([
+            { $match: { user_id: userId }},
+            {
+                $lookup: {
+                    from: "categories",
+                    localField: "category",
+                    foreignField: "_id",
+                    as: "categoryData"
+                }
+            },
+            {
+                $unwind: "$categoryData"
+            },
+            {
+                $project: {
+                    date: 1,
+                    category: "$categoryData.name",
+                    description: 1,
+                    amount: 1
+                }
+            }
+        ]).toArray();
 
-        const formattedMovements = movements.map(movement => ({
-            date: movement.date,
-            category: movement.category,
-            description: movement.description,
-            amount: movement.amount,
-        }));
-
-        res.json(formattedMovements);
+        res.json(movements);
     } catch (error) {
-        console.error("Failed to retrieve movements:", error);
-        res.status(500).send("Error retrieving movements data");
+        console.error("Failed to retrieve all movements:", error);
+        res.status(500).send("Error retrieving all movements data");
     }
 }
-
-
