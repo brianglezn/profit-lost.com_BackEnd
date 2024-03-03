440da4704854f81f17db4fa0811301815038903b
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-03-03 19:02:35 +0100

    03/03/24 *3
    lineas para depuración

--- src/controllers/usersController.mjs
+++ src/controllers/usersController.mjs
@@ -5,15 +5,17 @@ import { DB_NAME } from "../config/constants.mjs";
 const usersCollection = client.db(DB_NAME).collection("users");
 
 export async function getUserByToken(req, res) {
-    const userId = req.user.id;
-
-    console.log("Looking for userId:", userId); // Línea agregada para depuración
+    console.log("Getting user by ID...");
+    const userId = req.user.id; // Asegúrate de que esto coincide con cómo almacenaste el id en el token
+    console.log(`UserID from token: ${userId}`);
 
     try {
         const user = await usersCollection.findOne({ "_id": new ObjectId(userId) });
         if (user) {
+            console.log("User found:", user);
             res.json(user);
         } else {
+            console.log("User not found");
             res.status(404).send("User not found");
         }
     } catch (error) {
--- src/middlewares/authMiddleware.mjs
+++ src/middlewares/authMiddleware.mjs
@@ -2,15 +2,23 @@ import jwt from 'jsonwebtoken';
 import { JWT_KEY } from '../config/constants.mjs';
 
 export function authenticateToken(req, res, next) {
+    console.log("Verifying token...");
     const authHeader = req.headers['authorization'];
+    console.log(`Authorization Header: ${authHeader}`);
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) return res.sendStatus(401);
+    if (token == null) {
+        console.log("No token found");
+        return res.sendStatus(401);
+    }
 
     jwt.verify(token, JWT_KEY, (err, user) => {
-        if (err) return res.sendStatus(403);
+        if (err) {
+            console.log("Token verification failed", err);
+            return res.sendStatus(403);
+        }
 
-        console.log("User from token:", user); // Línea agregada para depuración
+        console.log("Token is valid, user:", user);
         req.user = user;
         next();
     });
