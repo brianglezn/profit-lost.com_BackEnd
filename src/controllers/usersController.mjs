09450f1cbf0aa65b121551cd1ad9e7f0a2c879c6
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-03-03 19:06:23 +0100

    03/03/24 *4
    prueba 1 getUserByToken

--- src/controllers/usersController.mjs
+++ src/controllers/usersController.mjs
@@ -5,21 +5,17 @@ import { DB_NAME } from "../config/constants.mjs";
 const usersCollection = client.db(DB_NAME).collection("users");
 
 export async function getUserByToken(req, res) {
-    console.log("Getting user by ID...");
-    const userId = req.user.id; // Asegúrate de que esto coincide con cómo almacenaste el id en el token
-    console.log(`UserID from token: ${userId}`);
+    const userId = req.user.userId; 
 
     try {
         const user = await usersCollection.findOne({ "_id": new ObjectId(userId) });
         if (user) {
-            console.log("User found:", user);
             res.json(user);
         } else {
-            console.log("User not found");
             res.status(404).send("User not found");
         }
     } catch (error) {
         console.error("Error retrieving user by token:", error);
-        res.status(500).send("Error retrieving user data by token");
+        res.status(500).send("Error retrieving user data");
     }
 }
