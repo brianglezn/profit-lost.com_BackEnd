9b0b84dc390ca816cd7cfdf025a955361438aa84
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-03-07 21:03:45 +0100

    07/03/24
    Creamos addMovement

--- src/controllers/movementsController.mjs
+++ src/controllers/movementsController.mjs
@@ -123,3 +123,85 @@ export async function getMovementsByYearAndMonth(req, res) {
         res.status(500).send("Error retrieving movements data by year and month");
     }
 }
+
+export async function addMovement(req, res) {
+    const userId = req.user.userId;
+    const { date, description, amount, category } = req.body;
+
+    if (!description || typeof amount !== 'number') {
+        return res.status(400).send('Invalid data provided');
+    }
+
+    if (!ObjectId.isValid(category)) {
+        return res.status(400).send('Invalid category ID');
+    }
+
+    const dateRegex = /^\d{4}-\d{2}$/;
+
+    if (!dateRegex.test(date)) {
+        return res.status(400).send('Date must be in format YYYY-MM');
+    }
+
+    try {
+        const newMovement = {
+            user_id: new ObjectId(userId),
+            date,
+            description,
+            amount,
+            category: new ObjectId(category)
+        };
+
+        const result = await movementsCollection.insertOne(newMovement);
+
+        res.status(201).json(result.ops[0]);
+    } catch (error) {
+        console.error("Error adding new movement:", error);
+        res.status(500).send("Error adding new movement");
+    }
+}
+
+export async function removeMovement(req, res) {
+    const { id } = req.params;
+
+    try {
+        const result = await movementsCollection.deleteOne({ _id: new ObjectId(id), user_id: new ObjectId(req.user.userId) });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).send("Movement not found");
+        }
+
+        res.status(200).send(`Movement with id ${id} deleted`);
+    } catch (error) {
+        console.error("Error removing movement:", error);
+        res.status(500).send("Error removing movement");
+    }
+}
+
+export async function editMovement(req, res) {
+    const { id } = req.params;
+    const { date, description, amount, category } = req.body;
+
+    try {
+        const updatedMovement = {
+            date,
+            description,
+            amount,
+            category: new ObjectId(category)
+        };
+
+        const result = await movementsCollection.findOneAndUpdate(
+            { _id: new ObjectId(id), user_id: new ObjectId(req.user.userId) },
+            { $set: updatedMovement },
+            { returnOriginal: false }
+        );
+
+        if (!result.value) {
+            return res.status(404).send("Movement not found");
+        }
+
+        res.json(result.value);
+    } catch (error) {
+        console.error("Error updating movement:", error);
+        res.status(500).send("Error updating movement");
+    }
+}
--- src/routes/authRoutes.mjs
+++ src/routes/authRoutes.mjs
@@ -2,19 +2,29 @@ import { Router } from "express";
 
 import { authenticateToken } from '../middlewares/authMiddleware.mjs';
 import { register, login } from "../controllers/authController.mjs";
-import { getAllMovements, getMovementsByYear, getMovementsByYearAndMonth } from '../controllers/movementsController.mjs';
+import { getAllMovements, getMovementsByYear, getMovementsByYearAndMonth, addMovement, removeMovement, editMovement } from '../controllers/movementsController.mjs';
 import { getAccountsByYear, getAllAccounts } from '../controllers/accountsController.mjs';
 import { getUserByToken } from '../controllers/usersController.mjs';
 
 const router = Router();
 
+// Rutas de autenticación
 router.post("/register", register);
 router.post("/login", login);
+
+// Rutas de movimientos
 router.get('/movements/all', authenticateToken, getAllMovements);
+router.post('/movements/add', authenticateToken, addMovement);
+router.delete('/movements/remove/:id', authenticateToken, removeMovement);
+router.put('/movements/edit/:id', authenticateToken, editMovement);
 router.get('/movements/:year', authenticateToken, getMovementsByYear);
 router.get('/movements/:year/:month', authenticateToken, getMovementsByYearAndMonth);
+
+// Rutas de cuentas
 router.get('/accounts/all', authenticateToken, getAllAccounts);
 router.get('/accounts/:year', authenticateToken, getAccountsByYear);
+
+// Rutas de usuarios
 router.get('/user/me', authenticateToken, getUserByToken);
 
 export default router;
