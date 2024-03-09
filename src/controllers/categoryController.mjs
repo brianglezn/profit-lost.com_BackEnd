5f2c045b92bc56c465b586044732ac9b67249c7e
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-03-09 15:02:05 +0100

    09/03/24 *7
    Prueba 1
    addCategory
    editCategory
    removeCategory

--- src/controllers/categoryController.mjs
+++ src/controllers/categoryController.mjs
@@ -17,3 +17,78 @@ export async function getAllCategories(req, res) {
         res.status(500).send("Error retrieving categories");
     }
 }
+
+export async function addCategory(req, res) {
+    const { name } = req.body;
+    const userId = req.user.userId;
+
+    if (!name) {
+        return res.status(400).send("Name is required");
+    }
+
+    try {
+        const newCategory = {
+            name,
+            user_id: new ObjectId(userId),
+        };
+
+        const result = await categoriesCollection.insertOne(newCategory);
+        res.status(201).json(result.ops[0]);
+    } catch (error) {
+        console.error("Error adding new category:", error);
+        res.status(500).send("Error adding new category");
+    }
+}
+
+export async function editCategory(req, res) {
+    const { id } = req.params;
+    const { name } = req.body;
+    const userId = req.user.userId;
+
+    if (!ObjectId.isValid(id)) {
+        return res.status(400).send("Invalid category ID format");
+    }
+
+    if (!name) {
+        return res.status(400).send("Name is required");
+    }
+
+    try {
+        const result = await categoriesCollection.findOneAndUpdate(
+            { _id: new ObjectId(id), user_id: new ObjectId(userId) },
+            { $set: { name } },
+            { returnDocument: 'after' }
+        );
+
+        if (!result.value) {
+            return res.status(404).send("Category not found");
+        }
+
+        res.json(result.value);
+    } catch (error) {
+        console.error("Error updating category:", error);
+        res.status(500).send("Error updating category");
+    }
+}
+
+export async function removeCategory(req, res) {
+    const { id } = req.params;
+    const userId = req.user.userId;
+
+    if (!ObjectId.isValid(id)) {
+        return res.status(400).send("Invalid category ID format");
+    }
+
+    try {
+        const result = await categoriesCollection.deleteOne({ _id: new ObjectId(id), user_id: new ObjectId(userId) });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).send("Category not found or does not belong to the user");
+        }
+
+        res.status(200).send(`Category with id ${id} deleted`);
+    } catch (error) {
+        console.error("Error removing category:", error);
+        res.status(500).send("Error removing category");
+    }
+}
--- src/routes/authRoutes.mjs
+++ src/routes/authRoutes.mjs
@@ -5,7 +5,7 @@ import { register, login } from "../controllers/authController.mjs";
 import { getAllMovements, getMovementsByYear, getMovementsByYearAndMonth, addMovement, removeMovement, editMovement } from '../controllers/movementsController.mjs';
 import { getAccountsByYear, getAllAccounts } from '../controllers/accountsController.mjs';
 import { getUserByToken } from '../controllers/usersController.mjs';
-import { getAllCategories } from '../controllers/categoryController.mjs';
+import { getAllCategories, addCategory, editCategory, removeCategory } from '../controllers/categoryController.mjs';
 
 const router = Router();
 
@@ -15,14 +15,17 @@ router.post("/login", login);
 
 // Rutas de movimientos
 router.get('/movements/all', authenticateToken, getAllMovements);
-router.post('/movements/add', authenticateToken, addMovement);
-router.delete('/movements/remove/:id', authenticateToken, removeMovement);
-router.put('/movements/edit/:id', authenticateToken, editMovement);
 router.get('/movements/:year', authenticateToken, getMovementsByYear);
 router.get('/movements/:year/:month', authenticateToken, getMovementsByYearAndMonth);
+router.post('/movements/add', authenticateToken, addMovement);
+router.put('/movements/edit/:id', authenticateToken, editMovement);
+router.delete('/movements/remove/:id', authenticateToken, removeMovement);
 
 // Rutas de categorias
 router.get('/categories/all', authenticateToken, getAllCategories);
+router.post('/categories/add', authenticateToken, addCategory);
+router.put('/categories/edit/:id', authenticateToken, editCategory);
+router.delete('/categories/remove/:id', authenticateToken, removeCategory);
 
 // Rutas de cuentas
 router.get('/accounts/all', authenticateToken, getAllAccounts);
