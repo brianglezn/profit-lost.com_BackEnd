b212a7f2c901bf81e4331a6a4faaf7008c7ace45
Brian González Novoa <119687340+brianglezn@users.noreply.github.com>
2024-11-22 18:07:36 +0100

    Eliminamos cron job backups

--- index.mjs
+++ index.mjs
@@ -1,13 +1,6 @@
 import express from "express";
 import cors from "cors";
-import cron from "node-cron";
-import { exec } from "child_process";
-import { join } from 'path';
-import { fileURLToPath } from "url";
 import authRoutes from "./src/routes/PLRoutes.mjs";
-import path from 'path';
-import { readdir, existsSync, mkdirSync, createReadStream } from 'fs';
-import { authenticateToken, authorizeBackupAccess } from "./src/middlewares/authMiddleware.mjs";
 import "dotenv/config";
 
 const app = express();
@@ -17,66 +10,6 @@ const PORT = process.env.PORT;
 
 app.use(authRoutes);
 
-app.get('/api/check-backups', authenticateToken, authorizeBackupAccess, async (req, res) => {
-    try {
-        const __filename = fileURLToPath(import.meta.url);
-        const __dirname = path.dirname(__filename);
-        const backupPath = join(__dirname, 'src/db_backups');
-        
-        if (!existsSync(backupPath)) {
-            mkdirSync(backupPath);
-        }
-
-        const files = await new Promise((resolve, reject) => {
-            readdir(backupPath, (err, files) => {
-                if (err) reject(err);
-                resolve(files);
-            });
-        });
-
-        res.json({ backups: files });
-    } catch (error) {
-        res.status(500).json({ error: 'Error reading backup directory', details: error.message });
-    }
-});
-app.get('/api/download-backup/:filename', authenticateToken, authorizeBackupAccess, (req, res) => {
-    try {
-        const __filename = fileURLToPath(import.meta.url);
-        const __dirname = path.dirname(__filename);
-        const backupPath = join(__dirname, 'src/db_backups');
-        const { filename } = req.params;
-        const file = join(backupPath, filename);
-
-        if (!existsSync(file)) {
-            return res.status(404).json({ error: 'File not found' });
-        }
-
-        res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
-        createReadStream(file).pipe(res);
-    } catch (error) {
-        res.status(500).json({ error: 'Error downloading file', details: error.message });
-    }
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const backupScriptPath = join(__dirname, 'src/config/db_bckp.mjs');
-
-cron.schedule('0 2 * * *', () => {
-  console.log('Executing backup job...');
-  exec(`node ${backupScriptPath}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing backup: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Backup stderr: ${stderr}`);
-      return;
-    }
-    console.log(`Backup completed successfully: ${stdout}`);
-  });
 });
--- src/middlewares/authMiddleware.mjs
+++ src/middlewares/authMiddleware.mjs
@@ -18,11 +18,3 @@ export function authenticateToken(req, res, next) {
         next();
     });
 }
-
-export function authorizeBackupAccess(req, res, next) {
-    const authorizedUserId = "65df4dfae27f115e23b1a1c2";
-    if (req.user.userId !== authorizedUserId) {
-        return res.sendStatus(403);
-    }
-    next();
-}
